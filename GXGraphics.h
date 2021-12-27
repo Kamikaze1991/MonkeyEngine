b738f84b264955df1e99e251a3680d04b2a0505c
Richi BPinargote <richyprg@gmail.com>
2021-12-27 14:03:22 -0500

    Creacion de la clase de graficos

---
+++ GXGraphics.h
@@ -0,0 +1,13 @@
+#ifndef _GXGRAPHICS_H_
+#define _GXGRAPHICS_H_
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
+class GXGraphics {
+private:
+public:
+	void initialize();
+};
+#endif
