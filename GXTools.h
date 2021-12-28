1720f93a61e474d0157db8da92f9f928b7363028
Richi BPinargote <richyprg@gmail.com>
2021-12-27 19:32:37 -0500

    create exception tool

---
+++ GXTools.h
@@ -0,0 +1,13 @@
+#ifndef _GXTOOLS_H_
+#define _GXTOOLS_H_
+
+#define GXManageException(x)                                              \
+{                                                                     \
+    HRESULT hr__ = (x);                                               \
+    std::wstring wfn = AnsiToWString(__FILE__);                       \
+    if(FAILED(hr__)) { throw DxException(hr__, L#x, wfn, __LINE__); } \
+}
+
+#endif
+
+
