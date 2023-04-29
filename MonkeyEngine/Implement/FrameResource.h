72515b92bfa774654b62c4e23a5c7e7ff3fadce2
Richi BPinargote <richyprg@gmail.com>
2023-04-29 09:43:37 -0500

    restructuracion alpha

--- MonkeyEngine/Implement/FrameResource.h
+++ MonkeyEngine/Implement/FrameResource.h
@@ -1,5 +1,6 @@
 #include "../Common/CoreUtil.h"
-struct FrameResource {
+class FrameResource {
+public:
 	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> mFrameCommandAllocator;
 	UINT64 mFrameFenceCount=0;
 	FrameResource(ID3D12Device* mDevice);
